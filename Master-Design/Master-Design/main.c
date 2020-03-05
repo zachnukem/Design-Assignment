66daaafffe01a43347881571b5166160a35f71fa
Rowan darlison <Rowan.darlison@gmail.com>
2020-03-05 13:38:17 -0400

    Initial Commit
    Created project

---
+++ Master-Design/Master-Design/main.c
@@ -0,0 +1,18 @@
+/*
+ * Master-Design.c
+ *
+ * Created: 2020-03-05 1:27:22 PM
+ * Author : Rowan Darlison
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
